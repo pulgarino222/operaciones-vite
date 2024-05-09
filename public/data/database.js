11f4317bdbaa96043952b53212c4f296e758cdcd
pulgarino222 <pulgarinhernandezjuanfelipe@gmail.com>
2024-05-09 11:24:38 -0500

    we start with forms

--- public/data/database.js
+++ public/data/database.js
@@ -17,154 +17,154 @@
         lastName: "bte",
         email: "caro@example.com"
     },
-    {
-        id: 4,
-        name: 'diego',
-        lastName: 'jaramillo',
-        email: 'diego06jp@hotmail.com'
-    },
-    {
-        id: 5,
-        name: "cristian",
-        lastName: "ortega",
-        email: "cristian@gmail.com"
-    },
-    {
-        id: 6,
-        name: "juan felipe",
-        lastName: "pulgarin",
-        email: "pulgarinhernandezjuanfelipe@gmail.com"
-    },
-    {
-        id: 7,
-        name: 'David',
-        lastName: 'Sanchez',
-        email: 'apolo4748@gmail.com',
-    },
-    {
-        id: 8,
-        name: 'juan',
-        lastName: 'pablo',
-        email: 'gomezjuanzero@gamil.com'
-    },
-    {
-        id: 9,
-        name: "nar",
-        lastName: "Mena",
-        email: "nar@example.com"
-    },
-    {
-        id: 10,
-        name: "Santiago",
-        lastName: "Pineda",
-        correo: "santiagopt97"
-    },
-    {
-        id: 11,
-        name: "daniela",
-        lastName: "londono",
-        email: "daniela0393h@gmail.com"
-    },
-    {
-        id: 12,
-        name: "pablo",
-        lastName: "ruiz",
-        email: "pabloruiz@outlook.com"
-    },
-    {
-        id: 13,
-        name: 'santiago',
-        lastName: 'Castro',
-        email: 'castrogiraldosantiago@gmail.com'
-    },
-    {
-        id: 14,
-        name: "karina",
-        lastName: "pineda",
-        email: "correok@email.com"
-    },
-    {
-        id: 15,
-        name: "brayan",
-        lastName: "rodriguez",
-        email: "brayanF@gmail.com"
-    },
-    {
-        id: 16,
-        name: "Marylin",
-        lastName: "restrepo",
-        email: "marylinrestrepp@gmail.com"
-    },
-    {
-        id: 17,
-        name: "alexis",
-        lastName: "soto",
-        email: "soto.alexis@hotmil.com"
-    },
-    {
-        id: 18,
-        name: "Juan camilo",
-        lastName: "Campillo",
-        email: "jancamilo@example.com"
-    },
-    {
-        id: 19,
-        name: "Juan José",
-        lastName: "Arenas Gaviria",
-        email: "juanjoarenas1218@gmail.com"
-    },
-    {
-        id: 20,
-        name: "Luis",
-        lastName: "Rubio",
-        email: "luis@example.com"
-    },
-    {
-        id: 21,
-        name: "oscar",
-        lastName: "muñoz",
-        email: "osmu1592@gmail.com"
-    },
-    {
-        id: 22,
-        name: "Esteban",
-        lastName: "Garcia",
-        email: "estebangccoder@gmail.com"
-    },
-    {
-        id: 23,
-        name: "oscar",
-        lastName: "muñoz",
-        email: "osmu1592@gmail.com"
-    },
-    {
-        id: 24,
-        name: "Marylin",
-        lastName: "Restrepo",
-        email: "marylinrestrepp@gmail.com"
-    },
-    {
-        id: 25,
-        name: "Edwin",
-        lastName: "Carmona",
-        email: "carmonaedwin1123@example.com"
-    },
-    {
-        id: 26,
-        name: "Camilo",
-        lastName: "Barreneche",
-        email: "camilito@gmail.com"
-    },
-    {
-        id: 27,
-        name: "marlon",
-        lastName: "peña",
-        email: "marlono1naranjo@gmail.com"
-    },
-    {
-        id: 28,
-        name: "andres",
-        lastName: "caceres",
-        email: "andres@gmail.com"
-    }
+    // {
+    //     id: 4,
+    //     name: 'diego',
+    //     lastName: 'jaramillo',
+    //     email: 'diego06jp@hotmail.com'
+    // },
+    // {
+    //     id: 5,
+    //     name: "cristian",
+    //     lastName: "ortega",
+    //     email: "cristian@gmail.com"
+    // },
+    // {
+    //     id: 6,
+    //     name: "juan felipe",
+    //     lastName: "pulgarin",
+    //     email: "pulgarinhernandezjuanfelipe@gmail.com"
+    // },
+    // {
+    //     id: 7,
+    //     name: 'David',
+    //     lastName: 'Sanchez',
+    //     email: 'apolo4748@gmail.com',
+    // },
+    // {
+    //     id: 8,
+    //     name: 'juan',
+    //     lastName: 'pablo',
+    //     email: 'gomezjuanzero@gamil.com'
+    // },
+    // {
+    //     id: 9,
+    //     name: "nar",
+    //     lastName: "Mena",
+    //     email: "nar@example.com"
+    // },
+    // {
+    //     id: 10,
+    //     name: "Santiago",
+    //     lastName: "Pineda",
+    //     correo: "santiagopt97"
+    // },
+    // {
+    //     id: 11,
+    //     name: "daniela",
+    //     lastName: "londono",
+    //     email: "daniela0393h@gmail.com"
+    // },
+    // {
+    //     id: 12,
+    //     name: "pablo",
+    //     lastName: "ruiz",
+    //     email: "pabloruiz@outlook.com"
+    // },
+    // {
+    //     id: 13,
+    //     name: 'santiago',
+    //     lastName: 'Castro',
+    //     email: 'castrogiraldosantiago@gmail.com'
+    // },
+    // {
+    //     id: 14,
+    //     name: "karina",
+    //     lastName: "pineda",
+    //     email: "correok@email.com"
+    // },
+    // {
+    //     id: 15,
+    //     name: "brayan",
+    //     lastName: "rodriguez",
+    //     email: "brayanF@gmail.com"
+    // },
+    // {
+    //     id: 16,
+    //     name: "Marylin",
+    //     lastName: "restrepo",
+    //     email: "marylinrestrepp@gmail.com"
+    // },
+    // {
+    //     id: 17,
+    //     name: "alexis",
+    //     lastName: "soto",
+    //     email: "soto.alexis@hotmil.com"
+    // },
+    // {
+    //     id: 18,
+    //     name: "Juan camilo",
+    //     lastName: "Campillo",
+    //     email: "jancamilo@example.com"
+    // },
+    // {
+    //     id: 19,
+    //     name: "Juan José",
+    //     lastName: "Arenas Gaviria",
+    //     email: "juanjoarenas1218@gmail.com"
+    // },
+    // {
+    //     id: 20,
+    //     name: "Luis",
+    //     lastName: "Rubio",
+    //     email: "luis@example.com"
+    // },
+    // {
+    //     id: 21,
+    //     name: "oscar",
+    //     lastName: "muñoz",
+    //     email: "osmu1592@gmail.com"
+    // },
+    // {
+    //     id: 22,
+    //     name: "Esteban",
+    //     lastName: "Garcia",
+    //     email: "estebangccoder@gmail.com"
+    // },
+    // {
+    //     id: 23,
+    //     name: "oscar",
+    //     lastName: "muñoz",
+    //     email: "osmu1592@gmail.com"
+    // },
+    // {
+    //     id: 24,
+    //     name: "Marylin",
+    //     lastName: "Restrepo",
+    //     email: "marylinrestrepp@gmail.com"
+    // },
+    // {
+    //     id: 25,
+    //     name: "Edwin",
+    //     lastName: "Carmona",
+    //     email: "carmonaedwin1123@example.com"
+    // },
+    // {
+    //     id: 26,
+    //     name: "Camilo",
+    //     lastName: "Barreneche",
+    //     email: "camilito@gmail.com"
+    // },
+    // {
+    //     id: 27,
+    //     name: "marlon",
+    //     lastName: "peña",
+    //     email: "marlono1naranjo@gmail.com"
+    // },
+    // {
+    //     id: 28,
+    //     name: "andres",
+    //     lastName: "caceres",
+    //     email: "andres@gmail.com"
+    // }
 ]
